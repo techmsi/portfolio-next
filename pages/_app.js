e484d2c906f4fdec153759710618fcddd2a54a89
techmsi <sophia.idro@gmail.com>
2021-07-15 20:16:53 -0400

    Initial

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import './app.scss';
+
+export default function Nextra({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ theme.config.js
@@ -0,0 +1,16 @@
+export default {
+  repository: 'https://github.com/techmsi/portfolio-next',
+  docsRepository: 'https://github.com/techmsi/portfolio-next',
+  branch: 'master',
+  path: '/',
+  titleSuffix: ' – Nextra',
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © Sophie.`,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next blog builder" />
+      <meta name="og:title" content="Nextra: the next blog builder" />
+    </>
+  ),
+};
